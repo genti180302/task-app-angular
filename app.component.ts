f0724b37ab1c7f1313be7f4cbabb79987a77490a
genti180302 <gentian_1@live.at>
2024-09-03 10:15:49 +0200

    finished project

---
+++ app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
+import { TaskComponent } from './tasks/task/task.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [HeaderComponent, UserComponent,TasksComponent,TaskComponent,NewTaskComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  users = DUMMY_USERS;
+  selectedUserId?: string;
+
+  get selectedUser () {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
+
+  onSelectUser(id:string){
+    this.selectedUserId = id;
+  }
+
+  /* onCompleteTask(id: string) {
+
+  } */
+}
---
+++ user/user.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Input, InputDecorator, Output} from '@angular/core';
+import { User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [CardComponent],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+
+
+export class UserComponent {
+  @Input({required:true}) user!: User;
+  @Input({required:true}) selected!: boolean;
+  @Output() select = new EventEmitter();
+ 
+
+  get ImagePath () {
+    return 'assets/users/' + this.user.avatar;
+  }
+  
+  onSelectUser () {
+    this.select.emit(this.user.id)
+  }
+}
